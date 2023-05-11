7c0347a090fcf64e591615a208f34b69b64a327e
Mikhail Kostomarov <kosmike@ukr.net>
2023-05-11 16:39:50 +0300

    Date check, update saved for displaying

--- Task8_Exam_Kostomarov.cpp
+++ Task8_Exam_Kostomarov.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 #define DATE 3
 #define WA 20
-using namespace std;
+using namespace std; 
 char* NM = nullptr;
 char* MN = nullptr;
 char* GR = nullptr;
@@ -21,10 +21,7 @@ struct Store
 
 Store input();
 void output(Store*);
-Store searchName(Store*, int, char*);
 int searchProdInd(Store*, int, char*);
-int searchProdIndmnf(Store*, int, char*);
-int searchProdIndgr(Store*, int, char*);
 void deleteprod(Store*, int, int);
 void updtProdname(Store&, char*);
 void updtProdmanf(Store&, char*);
@@ -87,7 +84,7 @@ int main()
 			char searchprod[WA];
 			cout << "Enter product name: ";
 			cin >> searchprod;
-			Store findProd = searchName(product, size, searchprod);
+			Store findProd = srchNm(product, size, searchprod);
 			output(&findProd);
 			int id = searchProdInd(product, size, searchprod);
 			cout << "\nDo you want to delete?\n1 - yes\n2 - no\n";
@@ -108,8 +105,9 @@ int main()
 			char searchprod[WA];
 			cout << "Enter product name: ";
 			cin >> searchprod;
-			Store findProd = searchName(product, size, searchprod);
+			Store findProd = srchNm(product, size, searchprod);
 			output(&findProd);
+			int id = searchProdInd(product, size, searchprod);
 			int searchway;
 			cout << "Choose category to edit:\n1 - name\n2 - manufacture\n3 - group\n4 - arrival date\n5 - expiry date\n6 - price\n";
 			cin >> searchway;
@@ -119,51 +117,71 @@ int main()
 				char nm[WA];
 				cout << "Enter name: ";
 				cin >> nm;
-				updtProdname(findProd, nm);
-				output(&findProd);
+				updtProdname(product[id], nm);
+				output(&product[id]);
 				break;
 			case 2:
 				char mnf[WA];
 				cout << "Enter manufacture: ";
 				cin >> mnf;
-				updtProdmanf(findProd, mnf);
-				output(&findProd);
+				updtProdmanf(product[id], mnf);
+				output(&product[id]);
 				break;
 			case 3:
 				char grp[WA];
 				cout << "Enter group: ";
 				cin >> grp;
-				updtProdgrp(findProd, grp);
-				output(&findProd);
+				updtProdgrp(product[id], grp);
+				output(&product[id]);
 				break;
 			case 4:
 				int arrv[DATE];
 				cout << "Enter arrival day: ";
 				cin >> arrv[0];
+				while (arrv[0] > 31 || arrv[0] < 1)
+				{
+					cout << "\nError value. Try again.\n";
+					cin >> arrv[0];
+				}
 				cout << "Enter arrival month: ";
 				cin >> arrv[1];
+				while (arrv[1] > 12 || arrv[1] < 1)
+				{
+					cout << "\nError value. Try again.\n";
+					cin >> arrv[1];
+				}
 				cout << "Enter arrival year: ";
 				cin >> arrv[2];
-				updtProdarrv(findProd, arrv);
-				output(&findProd);
+				updtProdarrv(product[id], arrv);
+				output(&product[id]);
 				break;
 			case 5:
 				int expr[DATE];
 				cout << "Enter expiry day: ";
 				cin >> expr[0];
+				while (expr[0] > 31 || expr[0] < 1)
+				{
+					cout << "\nError value. Try again.\n";
+					cin >> expr[0];
+				}
 				cout << "Enter expiry month: ";
 				cin >> expr[1];
+				while (expr[1] > 12 || expr[1] < 1)
+				{
+					cout << "\nError value. Try again.\n";
+					cin >> expr[1];
+				}
 				cout << "Enter expiry year: ";
 				cin >> expr[2];
-				updtProdexpr(findProd, expr);
-				output(&findProd);
+				updtProdexpr(product[id], expr);
+				output(&product[id]);
 				break;
 			case 6:
 				int prc;
 				cout << "Enter price: ";
 				cin >> prc;
-				updtProdprc(findProd, prc);
-				output(&findProd);
+				updtProdprc(product[id], prc);
+				output(&product[id]);
 				break;
 			default:
 				break;
@@ -186,13 +204,14 @@ int main()
 			case 2:
 				char mnfsrch[WA];
 				cout << "Enter manufacture: ";
-				cin >> nmsrch;
+				cin >> mnfsrch;
 				findProd = srchMnf(product, size, mnfsrch);
 				output(&findProd);
 				break;
 			case 3:
 				char grpsrch[WA];
 				cout << "Enter group: ";
+				cin >> grpsrch;
 				findProd = srchGrp(product, size, grpsrch);
 				output(&findProd);
 				break;
@@ -200,8 +219,18 @@ int main()
 				int arrdsrch[DATE];
 				cout << "Enter day ";
 				cin >> arrdsrch[0];
+				while (arrdsrch[0] > 31 || arrdsrch[0] < 1)
+				{
+					cout << "\nError value. Try again.\n";
+					cin >> arrdsrch[0];
+				}
 				cout << "Enter month ";
 				cin >> arrdsrch[1];
+				while (arrdsrch[1] > 12 || arrdsrch[1] < 1)
+				{
+					cout << "\nError value. Try again.\n";
+					cin >> arrdsrch[1];
+				}
 				cout << "Enter year ";
 				cin >> arrdsrch[2];
 				findProd = srchArrd(product, size, arrdsrch);
@@ -211,8 +240,18 @@ int main()
 				int expdsrch[DATE];
 				cout << "Enter day ";
 				cin >> expdsrch[0];
+				while (expdsrch[0] > 31 || expdsrch[0] < 1)
+				{
+					cout << "\nError value. Try again.\n";
+					cin >> expdsrch[0];
+				}
 				cout << "Enter month ";
 				cin >> expdsrch[1];
+				while (expdsrch[1] > 12 || expdsrch[1] < 1)
+				{
+					cout << "\nError value. Try again.\n";
+					cin >> expdsrch[1];
+				}
 				cout << "Enter year ";
 				cin >> expdsrch[2];
 				findProd = srchExprd(product, size, expdsrch);
@@ -285,11 +324,21 @@ Store input()
 		{
 			cout << "Enter day: ";
 			cin >> arrival1[i];
+			while (arrival1[i] > 31 || arrival1[i] < 1)
+			{
+				cout << "\nError value. Try again.\n";
+				cin >> arrival1[i];
+			}
 		}
 		else if (i == 1)
 		{
 			cout << "Enter month: ";
 			cin >> arrival1[i];
+			while (arrival1[i] > 12 || arrival1[i] < 1)
+			{
+				cout << "\nError value. Try again.\n";
+				cin >> arrival1[i];
+			}
 		}
 		else if (i == 2)
 		{
@@ -304,11 +353,21 @@ Store input()
 		{
 			cout << "Enter day: ";
 			cin >> expiry1[i];
+			while (expiry1[i] > 31 || expiry1[i] < 1)
+			{
+				cout << "\nError value. Try again.\n";
+				cin >> expiry1[i];
+			}
 		}
 		else if (i == 1)
 		{
 			cout << "Enter month: ";
 			cin >> expiry1[i];
+			while (expiry1[i] > 12 || expiry1[i] < 1)
+			{
+				cout << "\nError value. Try again.\n";
+				cin >> expiry1[i];
+			}
 		}
 		else if (i == 2)
 		{
@@ -341,26 +400,6 @@ void output(Store* product)
 	cout << "Price: " << product->price << endl;
 }
 
-Store searchName(Store* prod, int size, char* name)
-{
-	int match = 0;
-	for (int i = 0; i < size; i++)
-	{
-		if (strlen(prod[i].name) == strlen(name))
-		{
-			for (int j = 0; j < (strlen(name)); j++)
-			{
-				if (prod[i].name[j] == name[j])
-					match++;
-			}
-			if (match == strlen(name))
-				return prod[i];
-			match = 0;
-		}
-		else if ((i == size - 1) && match != strlen(name))
-			cout << "\nProduct name is absent\n";
-	}
-}
 
 int searchProdInd(Store* prod, int size, char* name)
 {
@@ -381,43 +420,6 @@ int searchProdInd(Store* prod, int size, char* name)
 	}
 }
 
-int searchProdIndmnf(Store* prod, int size, char* mn)
-{
-	int match = 0;
-	for (int i = 0; i < size; i++)
-	{
-		if (strlen(prod[i].manufacture) == strlen(mn))
-		{
-			for (int j = 0; j < (strlen(mn)); j++)
-			{
-				if (prod[i].manufacture[j] == mn[j])
-					match++;
-			}
-			if (match == strlen(mn))
-				return i;
-
-		}
-	}
-}
-
-int searchProdIndgr(Store* prod, int size, char* gr)
-{
-	int match = 0;
-	for (int i = 0; i < size; i++)
-	{
-		if (strlen(prod[i].group) == strlen(gr))
-		{
-			for (int j = 0; j < (strlen(gr)); j++)
-			{
-				if (prod[i].group[j] == gr[j])
-					match++;
-			}
-			if (match == strlen(gr))
-				return i;
-
-		}
-	}
-}
 
 void deleteprod(Store* prod, int id, int n)
 {
@@ -486,7 +488,7 @@ Store srchNm(Store* prod, int size, char* nm)
 				return prod[i];
 			match = 0;
 		}
-		else
+		else if ((i == size - 1) && match != strlen(nm))
 			cout << "\nName is absent\n";
 	}
 }
@@ -506,7 +508,7 @@ Store srchMnf(Store* prod, int size, char* mnf)
 				return prod[i];
 			match = 0;
 		}
-		else
+		else if ((i == size - 1) && match != strlen(mnf))
 			cout << "\nManufacture is absent\n";
 	}
 }
@@ -526,41 +528,47 @@ Store srchGrp(Store* prod, int size, char* grp)
 				return prod[i];
 			match = 0;
 		}
-		else
+		else if ((i == size - 1) && match != strlen(grp))
 			cout << "\nGroupe is absent\n";
 	}
 }
 Store srchArrd(Store* prod, int size, int* arrd)
 {
-	int match = 0;
+	
 	for (int i = 0; i < size; i++)
 	{
 		if (prod[i].arrival == arrd)
 		{
 			return prod[i];
 		}
+		else if ((i == size - 1) && prod[i].arrival != arrd)
+			cout << "\nArrival date is absent\n";
 	}
 }
 Store srchExprd(Store* prod, int size, int* axprd)
 {
-	int match = 0;
+	
 	for (int i = 0; i < size; i++)
 	{
 		if (prod[i].expiry == axprd)
 		{
 			return prod[i];
 		}
+		else if ((i == size - 1) && prod[i].expiry != axprd)
+			cout << "\nExpiry date is absent\n";
 	}
 }
 Store srchPrc(Store* prod, int size, int prc)
 {
-	int match = 0;
+	
 	for (int i = 0; i < size; i++)
 	{
 		if (prod[i].price == prc)
 		{
 			return prod[i];
 		}
+		else if ((i == size - 1) && prod[i].price != prc)
+			cout << "\nPrice is absent\n";
 	}
 }
 
